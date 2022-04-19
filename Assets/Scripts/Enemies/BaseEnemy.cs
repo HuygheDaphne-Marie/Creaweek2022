f38e65bbe553abc01ef99fa7ae84ed31235dd4c3
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2022-04-19 11:45:48 +0200

    Added CharacterController and Speed to BaseEnemy

--- Assets/Scripts/Enemies/BaseEnemy.cs
+++ Assets/Scripts/Enemies/BaseEnemy.cs
@@ -5,20 +5,25 @@ using UnityEngine;
 public abstract class BaseEnemy : MonoBehaviour
 {
     [SerializeField] protected string LayerName { get; private set; }
+    [SerializeField] protected int Speed { get; set; }
     protected int LayerID { get; private set; }
     protected BehaviourTree BehaviourTree { get; set; }
     protected Blackboard Blackboard { get; set; }
+    protected CharacterController CharacterController { get; private set; }
 
     private void Awake()
     {
         LayerID = LayerMask.GetMask(LayerName);
         Blackboard = new Blackboard();
+        CharacterController = gameObject.GetComponent<CharacterController>();
     }
 
     private void Start()
     {
         Blackboard.AddData("ThisEnemy", gameObject);
         Blackboard.AddData("MoveTarget", Vector3.zero);
+        Blackboard.AddData("EnemyCharacterController", CharacterController);
+        Blackboard.AddData("EnemySpeed", Speed);
     }
 
     private void Update()
