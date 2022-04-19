2e2e43171c0de942c5c9acc837aaf5c027bfd04e
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2022-04-19 12:30:58 +0200

    started on melee enemy

---
+++ Assets/Scripts/Enemies/MeleeEnemy.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class MeleeEnemy : BaseEnemy
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        /* Blackboard is made in BaseEnemy */
+        // BehaviourTree = new BehaviourTree(Blackboard, new BehaviourSelector
+        //     (
+        //     ));   
+    }
+}
